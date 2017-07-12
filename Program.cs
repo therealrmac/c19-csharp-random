95607426a2d8ae8e42d2e9b79c083dde410b381d
Ryan McCarty <Ryan McCarty>
2017-07-12 14:04:19 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Random rand= new Random();
+
+           List<int> num= new List<int>();
+     
+           for(var i=0; i<20; i++){
+               num.Add(rand.Next(1,50));
+           }         
+          
+
+          foreach(var x in num){
+              double y = Math.Pow(x,2);
+              //Console.WriteLine(y);
+              if(y % 2 ==0){
+                  Console.WriteLine(y);
+              }
+          }
+        }
+    }
+}
